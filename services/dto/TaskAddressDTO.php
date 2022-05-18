87d7f860660abdbb029add29674933f3dc02145b
mar4ehk0 <mar4ehk0@gmail.com>
2022-05-18 19:46:02 +0300

    #1 - Добавил DTO, для работы Task

---
+++ services/dto/TaskAddressDTO.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace app\services\dto;
+
+class TaskAddressDTO
+{
+    public string $location;
+    public string $address;
+    public ?int $city_id;
+    public string $lat;
+    public string $long;
+
+    public function __construct(
+        string $location,
+        string $address,
+        string $city_id,
+        string $lat,
+        string $long,
+    )
+    {
+        $this->location = $location;
+        $this->address = $address;
+        $this->city_id = empty($city_id) ? null : $city_id;
+        $this->lat = $lat;
+        $this->long = $long;
+    }
+}
