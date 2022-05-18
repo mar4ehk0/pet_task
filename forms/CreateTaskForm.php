a88f25a67708d7aaa67cec5b1365a572f602a9bb
mar4ehk0 <mar4ehk0@gmail.com>
2022-05-18 19:50:25 +0300

    #1 - Добавил CreateTaskForm для создания Task

--- forms/CreateTaskForm.php
+++ forms/CreateTaskForm.php
@@ -4,7 +4,6 @@ namespace app\forms;
 
 use app\models\Category;
 use app\models\City;
-use app\models\Client;
 use app\repositories\CategoryRepository;
 use app\repositories\CityRepository;
 use yii\base\Model;
@@ -14,13 +13,13 @@ class CreateTaskForm extends Model
 {
     public string $title = '';
     public string $description = '';
-    public int $category_id = 1;
+    public ?int $category_id = null;
     public string $location = '';
     public string $address = '';
-    public int $city_id = 1;
+    public string $city_id = '';
     public string $lat = '';
     public string $long = '';
-    public int $price = 1;
+    public string $price = '';
     public string $deadline = '';
     public bool $is_remote = false;
     public int $client_id;
@@ -44,11 +43,10 @@ class CreateTaskForm extends Model
     public function rules()
     {
         return [
-            [['title', 'description', 'category_id', 'client_id', 'price'], 'required'],
+            [['title', 'description', 'category_id', 'client_id'], 'required'],
             [['category_id', 'client_id'], 'integer'],
             [['title', 'description', 'location', 'address'], 'string'],
             [['lat', 'long'], 'string', 'max' => 100],
-            ['price', 'integer', 'min' => 1],
             ['is_remote', 'safe'],
             [['category_id'],
                 'exist',
@@ -61,6 +59,7 @@ class CreateTaskForm extends Model
                 'targetClass' => City::class,
                 'targetAttribute' => ['city_id' => 'id']],
             ['deadline', 'guardIsActualDate', 'skipOnEmpty' => false, 'skipOnError' => false],
+            ['price', 'guardIsValidPrice', 'skipOnEmpty' => false, 'skipOnError' => false],
         ];
     }
 
@@ -77,9 +76,10 @@ class CreateTaskForm extends Model
         ];
     }
 
-    public function guardIsActualDate($attribute, $params, $validator)
+    public function guardIsActualDate($attribute, $params, $validator): bool
     {
-        if (!isset($this->{$attribute})) {
+        if (empty($this->{$attribute})) {
+            $this->addError($attribute, 'Срок завершения не может быть пустым.');
             return false;
         }
 
@@ -89,7 +89,25 @@ class CreateTaskForm extends Model
             $this->addError($attribute, 'Срок завершения не может быть меньше текущей даты.');
             return false;
         }
+        return true;
+    }
 
+    public function guardIsValidPrice($attribute, $params, $validator): bool
+    {
+        if (!isset($this->{$attribute})) {
+            return false;
+        }
+
+        if (!empty($this->{$attribute})) {
+            if (!is_numeric($this->{$attribute})) {
+                $this->addError($attribute, 'Бдюжет может быть целым число.');
+                return false;
+            }
+            if ($this->{$attribute} < 0) {
+                $this->addError($attribute, 'Бдюжет не может быть отрицательным.');
+                return false;
+            }
+        }
         return true;
     }
 
