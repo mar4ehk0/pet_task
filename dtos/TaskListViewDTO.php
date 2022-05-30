5d9a4fe8db8d865f1b92ecc93f1dba864f1f2513
mar4ehk0 <mar4ehk0@gmail.com>
2022-05-30 19:32:45 +0300

    #5 - Добавил DTOs

---
+++ dtos/TaskListViewDTO.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace app\dtos;
+
+use app\helpers\TaskView;
+use app\models\Task;
+use yii\data\Pagination;
+
+class TaskListViewDTO
+{
+    /**
+     * @var TaskView[]
+     */
+    public array $tasks;
+    public Pagination $pages;
+
+    /**
+     * @param Task[] $tasks
+     * @param Pagination $pages
+     */
+    public function __construct(array $tasks, Pagination $pages)
+    {
+        $this->tasks = [];
+        foreach ($tasks as $task) {
+            $this->tasks[] = new TaskView($task);
+        }
+        $this->pages = $pages;
+    }
+}
