1158d74c20d17dec4c5f967bc75c9021b9db9d87
mar4ehk0 <mar4ehk0@gmail.com>
2022-05-30 19:34:34 +0300

    #5 - Изменил FindTaskForm

--- forms/FindTaskForm.php
+++ forms/FindTaskForm.php
@@ -2,6 +2,7 @@
 
 namespace app\forms;
 
+use app\dtos\PeriodDTO;
 use app\helpers\ViewHelper;
 use app\repositories\CategoryRepository;
 use yii\base\Model;
@@ -10,18 +11,27 @@ use yii\helpers\ArrayHelper;
 class FindTaskForm extends Model
 {
 
+    public const STATUS_ANY = ['any' => 'Любой'];
+    public const PERIOD_ANY = ['any' => 'Любой'];
+
     public array $categories = [];
-    public array $status = [];
+    public string $status = '';
     public string $period = '';
+    private int $client_id = 0;
+
 
     private CategoryRepository $categoryRepository;
-    private array $get;
 
-    public function __construct(CategoryRepository $categoryRepository, array $get, $config = [])
+    public function __construct(CategoryRepository $categoryRepository, int $client_id, array $get, $config = [])
     {
         parent::__construct($config);
         $this->categoryRepository = $categoryRepository;
-        $this->get = $get;
+        $this->period = key(self::PERIOD_ANY);
+        if (!empty($get['FindTaskForm'])) {
+            $this->setDefault($get['FindTaskForm']);
+        }
+
+        $this->client_id = $client_id;
     }
 
     public function rules()
@@ -49,21 +59,73 @@ class FindTaskForm extends Model
 
     public function getPeriodList(): array
     {
-        return [
-            '5 second' => 'Только что',
-            '1 hour' => '1 час',
-            '2 hour' => '2 часа',
-            '4 hour' => '4 часа',
-            '8 hour' => '8 часов',
-            '12 hour' => '12 часов',
-            '24 hour' => '24 часа',
-            '2 day' => '2 дня',
-            '3 day' => '3 дня',
-        ];
+        return array_merge(
+            self::PERIOD_ANY,
+            [
+                '1 minute' => 'Только что',
+                '1 hour' => '1 час',
+                '2 hour' => '2 часа',
+                '4 hour' => '4 часа',
+                '8 hour' => '8 часов',
+                '12 hour' => '12 часов',
+                '24 hour' => '24 часа',
+                '2 day' => '2 дня',
+                '3 day' => '3 дня',
+            ]
+        );
     }
 
     public function getListStatus(): array
     {
-        return ViewHelper::getListStatusTask();
+        return array_merge(self::STATUS_ANY, ViewHelper::getListStatusTask());
+    }
+
+    public function getCategoriesId(): ?array
+    {
+        if (empty($this->categories)) {
+            return null;
+        }
+
+        return array_keys($this->categories);
+    }
+
+    public function getStatus(): ?string
+    {
+        if ($this->status === key(self::STATUS_ANY)) {
+            return null;
+        }
+        return $this->status;
+    }
+
+    public function getPeriod(): ?PeriodDTO
+    {
+        if ($this->period === key(self::STATUS_ANY)) {
+            return null;
+        }
+        $from = new \DateTime();
+        $to = new \DateTime($this->period);
+        return  new PeriodDTO($from->format('Y-m-d'),$to->format('Y-m-d'));
+    }
+
+    private function setDefault(array $get): void
+    {
+        if (!empty($get['categories'])) {
+            $this->categories = $get['categories'];
+        }
+        if (!is_null($get['status'])) {
+            $this->status = $get['status'];
+        }
+        if (!empty($get['period'])) {
+            $this->period = $get['period'];
+        }
+    }
+
+    public function getClientId(): ?int
+    {
+        if ($this->client_id === 0) {
+            return null;
+        }
+
+        return $this->client_id;
     }
 }
